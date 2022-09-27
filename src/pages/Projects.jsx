d62a816c2ec2aa5e0dfd0a6cf8a0956c0e443ef4
colinmyers37 <colinmyers37@gmail.com>
2022-09-27 14:35:00 -0500

    deploy

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -49,7 +49,6 @@ const Projects = () => {
             </ProjectParagraphContainer>
           </ProjectCards>
           
-         
           
         </ProjectWrapper>
       </ProjectsContainerStyled>
