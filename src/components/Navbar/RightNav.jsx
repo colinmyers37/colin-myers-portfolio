6a7be63137014503103da7463dee3b2067532706
colinmyers37 <colinmyers37@gmail.com>
2022-09-28 12:40:39 -0500

    updated project to projects

--- src/components/Navbar/RightNav.jsx
+++ src/components/Navbar/RightNav.jsx
@@ -11,7 +11,7 @@ const RightNav = ({ isOpen, toggle }) => {
         <RightNavStyled isOpen={isOpen} onClick={toggle}>
             <li><LinkStyled to='/' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>Home</LinkStyled></li>
             <li><LinkStyled to='about' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>About</LinkStyled></li>
-            <li><LinkStyled to='projects' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>Project</LinkStyled></li>
+            <li><LinkStyled to='projects' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>Projects</LinkStyled></li>
             <li><LinkStyled to='contact' onClick={toggle}smooth={true} duration={500} spy={true} exact={true} offset={-80}>Contact</LinkStyled></li>
         </RightNavStyled>
       </div>
