a3af45ba4d26c7044b672ec4536fd9cff7bd341a
colinmyers37 <colinmyers37@gmail.com>
2022-09-29 13:23:42 -0500

    added resume tab

--- src/components/Navbar/RightNav.jsx
+++ src/components/Navbar/RightNav.jsx
@@ -1,22 +1,80 @@
-import React from 'react';
-import { LinkStyled,  RightNavStyled } from '../../styles/Nav'
-
-
+import React from "react";
+import { LinkStyled, RightNavStyled } from "../../styles/Nav";
 
 const RightNav = ({ isOpen, toggle }) => {
+  function resumeClick() {
+    console.log("resume click is working");
+    window.open(
+      "https://drive.google.com/file/d/1viD752As63oMWKzejh3rFPrS_DxHelPU/view?usp=sharing",
+      "_blank"
+    );
+  }
 
-    return(
-      <> 
+  return (
+    <>
       <div>
         <RightNavStyled isOpen={isOpen} onClick={toggle}>
-            <li><LinkStyled to='/' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>Home</LinkStyled></li>
-            <li><LinkStyled to='about' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>About</LinkStyled></li>
-            <li><LinkStyled to='projects' onClick={toggle} smooth={true} duration={500} spy={true} exact={true} offset={-80}>Projects</LinkStyled></li>
-            <li><LinkStyled to='contact' onClick={toggle}smooth={true} duration={500} spy={true} exact={true} offset={-80}>Contact</LinkStyled></li>
+          <li>
+            <LinkStyled
+              to="/"
+              onClick={toggle}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              Home
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled
+              to="about"
+              onClick={toggle}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              About
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled
+              to="projects"
+              onClick={toggle}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              Projects
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled
+              to="contact"
+              onClick={toggle}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              Contact
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled to="#" onClick={resumeClick}>
+              Resume
+            </LinkStyled>
+          </li>
         </RightNavStyled>
       </div>
-      </>
-    )
-}
+    </>
+  );
+};
 
 export default RightNav;
